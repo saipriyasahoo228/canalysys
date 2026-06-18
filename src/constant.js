8c480c9dcc610fc9cdad85956d8ec8b9b12e670a
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2026-06-18 10:08:10 +0530

    base url

--- src/constant.js
+++ src/constant.js
@@ -22,9 +22,23 @@
 
 
 
+// const DEFAULT_API = {
+//   development: 'https://apicartest.intulet.com',
+//   production: 'https://apicartest.intulet.com'
+// };
+
+// // Vite uses import.meta.env instead of process.env
+// const API_URI = import.meta.env.VITE_API_URL || 
+//                 DEFAULT_API[import.meta.env.MODE] || 
+//                 DEFAULT_API.development;
+
+// export const API_BASE_URL = API_URI;
+
+
+
 const DEFAULT_API = {
-  development: 'https://apicartest.intulet.com',
-  production: 'https://apicartest.intulet.com'
+  development: 'https://testapi.carnalysys.com',
+  production: 'https://testapi.carnalysys.com'
 };
 
 // Vite uses import.meta.env instead of process.env
@@ -33,6 +47,9 @@ const API_URI = import.meta.env.VITE_API_URL ||
                 DEFAULT_API.development;
 
 export const API_BASE_URL = API_URI;
+
+
+
 
 
 

3bbf099bc4a3dd2a71cb53e1ee387307e1a054a6
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2026-08-03 12:24:41 +0530

    test api

--- src/constant.js
+++ src/constant.js
@@ -22,23 +22,9 @@
 
 
 
-const DEFAULT_API = {
-  development: 'https://api.carnalysys.com',
-  production: 'https://api.carnalysys.com'
-};
-
-// Vite uses import.meta.env instead of process.env
-const API_URI = import.meta.env.VITE_API_URL || 
-                DEFAULT_API[import.meta.env.MODE] || 
-                DEFAULT_API.development;
-
-export const API_BASE_URL = API_URI;
-
-
-
 // const DEFAULT_API = {
-//   development: 'https://testapi.carnalysys.com',
-//   production: 'https://testapi.carnalysys.com'
+//   development: 'https://api.carnalysys.com',
+//   production: 'https://api.carnalysys.com'
 // };
 
 // // Vite uses import.meta.env instead of process.env
@@ -47,6 +33,20 @@ export const API_BASE_URL = API_URI;
 //                 DEFAULT_API.development;
 
 // export const API_BASE_URL = API_URI;
+
+
+
+const DEFAULT_API = {
+  development: 'https://testapi.carnalysys.com',
+  production: 'https://testapi.carnalysys.com'
+};
+
+// Vite uses import.meta.env instead of process.env
+const API_URI = import.meta.env.VITE_API_URL || 
+                DEFAULT_API[import.meta.env.MODE] || 
+                DEFAULT_API.development;
+
+export const API_BASE_URL = API_URI;
 
 
 
